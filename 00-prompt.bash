d74cbd95e17642de6624470b9158f7b8d69a2b2c
MD Support <399047+mdsupport@users.noreply.github.com>
2023-08-01 19:05:55 -0700

    Fix command-line overwrite issue
    Enclose non-printable characters by '\[' and '\]'.

--- 00-prompt.bash
+++ 00-prompt.bash
@@ -1,4 +1,8 @@
+# Caution : Terminal looses track of characters printed on a line
+# if non-printable characters are not enclosed by '\[' and '\]'.
+#
 # Ref : https://www.codeproject.com/Articles/5329247/How-to-Change-Text-Color-in-a-Linux-Terminal
+#
 # Foreground Color - "\033[" + "<0 or 1, meaning normal or bold>;" + "<color code> + "m"
 function FG {
   local HL=0
@@ -15,17 +19,17 @@ function BG {
 # Local prompt
 function SET_PS12 {
   # DarkRed on Yellow
-  local TXT='$(FG 0 31)$(BG 103)'
-  local RESET='$(BG 0)'
+  local ESC_T='\[$(FG 0 31)$(BG 103)\]'
+  local RESET='\[$(BG 0)\]'
 
   # Enhanced prompt elements
-  local PS_T="$TXT\t$RESET"
-  local PS_U="$TXT\u$RESET"
-  local PS_H="$TXT\h$RESET"
-  local PS_W="$TXT\w$RESET"
+  local PS_T="$ESC_T\t$RESET"
+  local PS_U="$ESC_T\u$RESET"
+  local PS_H="$ESC_T\h$RESET"
+  local PS_W="$ESC_T\w$RESET"
 
-  PS1="$PS_T $PS_U@$PS_H $PS_W > "
-  PS2="Contd.$TXT->$RESET "
+  PS1="$PS_T $PS_U@$PS_H $PS_W > $RESET"
+  PS2="Contd.$ESC_T-> $RESET"
 }
 
 SET_PS12
